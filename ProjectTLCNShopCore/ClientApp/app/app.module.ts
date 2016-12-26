1d26de59d3ed556f34a5e7f418cf54e5e41785c1
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2016-12-26 14:10:26 +0700

    Fix error font

--- ProjectTLCNShopCore/ClientApp/app/app.module.ts
+++ ProjectTLCNShopCore/ClientApp/app/app.module.ts
@@ -3,11 +3,11 @@ import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
 
-
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [ AppComponent],
     declarations: [
         AppComponent,
+        
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
--- ProjectTLCNShopCore/ClientApp/app/components/app/app.component.ts
+++ ProjectTLCNShopCore/ClientApp/app/components/app/app.component.ts
@@ -1,35 +1,9 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-
+ï»¿import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: require('./app.component.html'),
-    styles: [require('./app.component.css')]
+    //templateUrl:'test.component.html'
 })
 export class AppComponent {
-    //title = 'My Shop';
-    //hero = 'Windstorm';
-    //public cate: catergories[];
-
-    //constructor(http: Http) {
-    //    http.get('/api/categories').subscribe(result => {
-    //        this.cate = result.json();
-    //    });
-    //}
-    public movieList = ['Công ngh? PM', 'Th??ng m?i ?i?n t?', 'C?u trúc d? li?u'];
-    selectedMovie() {
-        alert('amoive was selected!');
-    }
-
-}
-
-
-interface catergories {
-    categoryId: number;
-    categoryName: string;
-    description: string;
-    picture: string;
-    displayOrder: number;
-    isDisplay: boolean;
-    products: any[];
+    name = 'Nguyá»…n Thanh An';
 }
